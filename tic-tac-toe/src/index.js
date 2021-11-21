285c5d5795a6400f6952f7b990a488fda9c2b394
marceloserpa <jdev.marcelo@gmail.com>
2021-11-21 10:23:18 -0300

    add circle on tictactoe

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
 
-
-  render() {
-    return (
-      <button className="square" 
-              onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -27,13 +23,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'x';
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   renderSquare(i) {
@@ -44,8 +44,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O' }`;
     return (
       <div>
         <div className="status">{status}</div>
