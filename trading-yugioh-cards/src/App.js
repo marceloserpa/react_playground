6997b437b11e37989e493f7215c63616a14af826
marceloserpa <jdev.marcelo@gmail.com>
2021-11-21 11:18:41 -0300

    yugioh store: initial commit

---
+++ trading-yugioh-cards/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import './Card.js';
+import Card from './Card.js';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <div class = "container">
+
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+      </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ trading-yugioh-cards/src/Card.js
@@ -0,0 +1,14 @@
+import './Card.css';
+
+function Card() {
+  return (
+    <div className="Card">
+        <h3>Dark Armed Dragon SDDC-EN012</h3>
+        <img src="https://www.solosagrado.com.br/images/produtos/w200/h292/22078.jpg" />
+        <p>price: R$ 19,00</p>
+        <button>Buy</button>
+    </div>
+  );
+}
+
+export default Card;
